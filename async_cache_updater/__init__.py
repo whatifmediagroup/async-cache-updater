cb894c2fe3494c92e872fcfc6ca18f18d8a15f7a
Ryan Anguiano <ryan@jangl.com>
2021-09-21 13:22:57 -0400

    Bump version: 0.1.3 → 0.1.4

--- async_cache_updater/__init__.py
+++ async_cache_updater/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """RevPoint Media"""
 __email__ = 'tech@jangl.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 __all__ = ['async_cache_updater', 'cache_settings', 'setup_client']
 
