08db6cf1b1e8b8475fd79ae33eef4f6dae29e243
Ryan Anguiano <ryan@jangl.com>
2025-08-14 00:35:06 -0400

    Bump version: 0.1.5 → 1.0.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/revpoint/async-cache-updater',
-    version='0.1.5',
+    version='1.0.0',
     zip_safe=False,
 )
